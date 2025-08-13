9ad441481f19d1314376e248383dd518430da6a5
kimjihee <jiyi9976@gmailc.om>
2025-08-13 18:54:27 +0900

    add_join/leave_group_try_catch

--- src/join_group.js
+++ src/join_group.js
@@ -14,22 +14,25 @@ app.use(express.json());
 app.post(`/groups/:groupId/participants`, async(req, res) => {
   const groupId = parseInt(req.params.groupId);
   const { nickname, password } = req.body;
-  const existingParticipants = await prisma.participant.findUnique({
-    where: { nickname },
-  });
-  
-  if (existingParticipants === null){
-    const newParticipants = await prisma.participant.create({
+  try{
+    const existingParticipants = await prisma.participant.findUnique({
+      where: { nickname },
+    });
+    if (existingParticipants === null){
+      const newParticipants = await prisma.participant.create({
         data: {
-            groupId, 
-            nickname, 
-            password,
-          }
-        });
-    console.log(`${nickname}은 사용가능합니다. 축하합니다. 그룹 참여가 가능합니다.`)
-    return res.status(201).json(newParticipants);
+          nickname,
+          password,
+        },
+      });
+      console.log(`${nickname}은 사용가능합니다. 축하합니다. 그룹 참여가 가능합니다.`);
+      return res.status(201).json(newParticipants);
     } else {
-    console.log(`${nickname}은 이미 존재하는 닉네임입니다. 다른 닉네임을 사용하세요.`);
-    return res.status(400).json({ error: `${nickname}은 이미 존재하는 닉네임입니다. 다른 닉네임을 사용하세요.`})
+      console.log(`${nickname}은 이미 존재하는 닉네임입니다. 다른 닉네임을 사용하세요.`);
+      return res.status(400).json({ error: `${nickname}은 이미 존재하는 닉네임입니다. 다른 닉네임을 사용하세요.`});
     }
+  } catch(error){
+    console.error('서버 오류로 닉네임 생성이 불가합니다.');
+    return res.status(500).json({ message: '서버 오류로 닉네임 생성이 불가합니다.'});
+  }
 });
--- src/leave_group.js
+++ src/leave_group.js
@@ -5,27 +5,32 @@ const app = express()
 const prisma = new PrismaClient;
 
 app.delete('/groups/:groupId/participants', async(req,res) => {
-    const  groupId  = parseInt(req.params.groupId);
-    const { nickname, password } = req.body;
-  
-    const existingParticipant = await prisma.participant.findUnique({
-        where: { 
-          participantInfo : {
-          nickname, 
-          password,
-          },
-        }
-      })
-    if(existingParticipant === null){
-        console.log('닉네임과 비밀번호가 일치하지 않습니다. 다시 입력해주세요.')
-        return res.status(400).json({ error: '닉네임과 비밀번호가 일치하지 않습니다. 다시 입력해주세요.'});
-    } else {
-        await prisma.participant.delete({
+  const  groupId  = parseInt(req.params.groupId);
+  const { nickname, password } = req.body;
+  try{
+    const existingParticipant = await prisma.participant.findunique({
+      where: {
+        participantInfo: { 
+          nickname,
+          password
+        },
+      }
+    })
+    if(existingParticipant !== null){
+      await prisma.participant.delete({
         where: {
-          id: existingParticipant.id,
+          id: existingParticipant.id
         }
       })
+      console.log('그룹을 탈퇴했습니다.');
+      return res.status(204).send();
+    } else {
+      console.log('닉네임, 비밀번호가 일치하지 않습니다. 다시 입력해주세요.');
+      return res.status(400).json({message: '닉네임, 비밀번호가 일치하지 않습니다.'});
     }
-    console.log('해당 참여자는 그룹을 탈퇴했습니다.')
-    return res.status(204).send()
-  });
+  } catch(error){
+    console.error('서버 오류로 탈퇴 불가합니다..');
+    return res.status(500).json({message: '서버오류로 탈퇴 불가합니다.'});
+  }
+});
+    
