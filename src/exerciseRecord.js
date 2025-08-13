fbfceaf36aeb245e17b54556a6155e09f5d7951e
kimjihee <jiyi9976@gmailc.om>
2025-08-13 16:49:01 +0900

    change_schema

--- src/exerciseRecord.js
+++ src/exerciseRecord.js
@@ -3,7 +3,6 @@ import express from 'express';
 
 const prisma = new PrismaClient();
 
-<<<<<<< HEAD
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -25,29 +24,10 @@ app.post('/:groupId/records', async(req, res) => {
                 participantId,
                 participantNickname,
                 }
-=======
-app.use(express.json());
-
-app.post('/:groupId/records', async(req, res) => {
-    try {
-        const groupId = Number(req.params.groupId);
-        const { exerciseType, description, time, distance, photos, participantNickname, participantPassword } = req.body;
-        const record = await prisma.record.create({
-            data: {
-            groupId: groupId,
-            exerciseType,
-            description,
-            time,
-            distance,
-            photos,
-            participantNickname,
-            participantPassword,
->>>>>>> 16bd529 (Modified schema & try create exerciseRecord.js)
             },
         });
         return res.status(201).json(record);
     } catch (err) {
-<<<<<<< HEAD
         return res.status(400).json({
         "path": "groupId",
         "message": "groupId must be integer"     
@@ -114,92 +94,5 @@ app.get('/:groupId/records/:recordId', async(req, res) => {
 });
 
 app.listen(port, () => {
-=======
-        return res.status(400).json({ 
-            "path": "groupId",
-            "message": "groupId must be integer"
-        });
-    }
-  })
-
-app.get('/:groupId/records', async(req, res) => {
-    try {
-        const groupId = Number(req.params.groupId);
-        const page = parseInt(req.query.page) || 1;
-        const keyword = req.query.keyword || '';
-        const record = await prisma.record.findMany({
-            skip: (page - 1) * 10,
-            take: 10,
-            orderBy: [
-                { time: 'desc' },
-                { createdAt: 'desc' },
-            ],
-            select: {
-              id: true,  
-              exerciseType: true,
-              description: true,
-              time: true,
-              distance: true,
-              photos: true,
-              author: {
-                participantId: true,
-                participantNickname: true,
-              }
-            },
-            where: {
-              groupId: groupId,
-                 participantNickname: {
-                contain: keyword,
-                mode: 'insensitive',
-            }
-         },
-        });
-        res.status(201).json(record);
-    } catch (err) {
-        res.status(400).json({ 
-            "path": "groupId",
-  "message": "groupId must be integer"
-        })
-    }
-  });
-
-app.get('/:groupId/records/:recordId', async(req, res) => {
-    try {
-        const groupId = Number(req.params.groupId);
-        const recordId = Number(req.params.recordId);
-        const page = parseInt(req.query.page) || 1;
-        const keyword = req.query.keyword || '';
-        const record = await prisma.record.findMany({
-            select: {
-              id: true,  
-              exerciseType: true,
-              description: true,
-              time: true,
-              distance: true,
-              photos: true,
-              author: {
-                participantId: true,
-                participantNickname: true,
-              }
-            },
-            where: {
-              groupId: groupId,
-                 participantNickname: {
-                contain: keyword,
-                mode: 'insensitive',
-            }
-         },
-        });
-        res.status(201).json(record);
-    } catch (err) {
-        res.status(400).json({ 
-            "path": "groupId",
-  "message": "groupId must be integer"
-        })
-    }
-  });
-
-  app.listen(port, () => {
->>>>>>> 16bd529 (Modified schema & try create exerciseRecord.js)
   console.log(`Server started at port: ${port}`);
 });
