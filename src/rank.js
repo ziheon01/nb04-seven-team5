94622fe49694dd859f38e11e946448303e87bf1f
양정민 <alpp7123@gmail.com>
2025-08-19 15:26:20 +0900

    refactor: 경로&변수명 수정, ranking에 select 필드 모두 명시

--- src/rank.js
+++ src/rank.js
@@ -11,12 +11,13 @@ app.use(express.json());
 
 /*
 조건문에 쿼리를 명시하기 위해 받아오는 문자열을 대문자로 변환해 변수로 할당.
-객체로 할당시 {COUNT, DURATION} - { COUNT : 'count', DURATION : 'duration' }
+객체로 할당시 {COUNT, DURATION} - { COUNT : 'count', Time : 'time' }
 */
-const [COUNT, DURATION] = ['count', 'duration']
+const [COUNT, TIME] = ['count', 'time']
+
+const groupId = Number(req.params.groupId)
 
 async function getRanks(req, res, next) {
-    const groupId = Number(req.params.groupId)
     const ranking = req.query.ranking;
     //잘못된 ID접근에 대해 early return
     if (!groupId || isNaN(groupId)) {
@@ -34,6 +35,7 @@ async function getRanks(req, res, next) {
                 select: {
                     participantId: true,
                     nickname: true,
+                    recordTime: true,
                     recordCount: true
                 },
                 where: {
@@ -45,12 +47,13 @@ async function getRanks(req, res, next) {
             })
             return res.json(result);
         }
-        if (ranking === DURATION) {
+        if (ranking === TIME) {
             result = await prisma.participant.findMany({
                 select: {
                     participantId: true,
                     nickname: true,
-                    recordTime: true
+                    recordTime: true,
+                    recordCount: true,
                 },
                 where: {
                     groupId
@@ -63,14 +66,14 @@ async function getRanks(req, res, next) {
         }
         //잘못된 쿼리 요청에 대한 응답
         return res.status(400).json({
-            error: "ranking 쿼리 파라미터는 'count' 또는 'duration'이어야 합니다."
+            error: "ranking 쿼리 파라미터는 'count' 또는 'time'이어야 합니다."
         });
         //next(); 추가 쿼리가 있거나 확장성이 필요할 경우 위의 에러 처리대신 다음 미들웨어로 컨트롤
-    } catch(err) {
+    } catch (err) {
         next(err)
-        }
     }
+}
 
 
 
-app.get(`/groups/:groupId/participants`, getRanks)
+app.get(`/groups/${groupId}/rank`, getRanks)
